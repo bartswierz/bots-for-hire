e451cee8814d7f5280d557575237fd492e087878
bartswierz <bartoszswierzynski7@gmail.com>
2022-11-21 16:42:48 -0600

    added bookmark logic to add them to hire list

--- src/components/bookmark-list/bookmark-list.component.jsx
+++ src/components/bookmark-list/bookmark-list.component.jsx
@@ -1,5 +1,6 @@
 import "./bookmark-list.styles.scss";
 import { BookmarkContext } from "../../contexts/bookmark.context";
+import { HiredContext } from "../../contexts/hired.context";
 import { useContext } from "react";
 
 // export const HiredList = ({ user }) => {
@@ -7,6 +8,15 @@ export const BookmarkList = () => {
   // Hired List holds our current list
   // const { hiredList, hiredCount, removeHireFromList } = useContext(HiredContext);
   const { bookmarkList, bookmarkCount, removeFromBookmarkList } = useContext(BookmarkContext);
+  const { addHireToList } = useContext(HiredContext);
+
+  // Passing in user info as object, user = id, first_name, last_name, avatar
+  const hireBookmarkHandler = (user) => {
+    //add to hirelist
+    addHireToList(user);
+    //remove from bookmark list
+    removeFromBookmarkList(user.id);
+  };
 
   return (
     <div className="bookmark-container">
@@ -20,8 +30,15 @@ export const BookmarkList = () => {
               <div className="bookmark-text">
                 {first_name} {last_name} <img src={avatar} className="bookmark-avatar" alt="Employee"></img>
                 {/* Add a Checkmark to ADD to HIRE LIST */}
+                <div>
+                  {/* TODO: onClick, add to hired list AND remove from bookmark list */}
+                  <ion-icon
+                    onClick={() => hireBookmarkHandler({ id, first_name, last_name, avatar })}
+                    name="checkmark-sharp"
+                    color="light"
+                  ></ion-icon>
+                </div>
                 <div className="remove-btn">
-                  {/* <ion-icon name="trash-outline"></ion-icon> */}
                   <ion-icon onClick={() => removeFromBookmarkList(id)} name="trash-outline"></ion-icon>
                 </div>
               </div>
